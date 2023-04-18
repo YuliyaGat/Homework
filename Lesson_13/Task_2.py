847801790ec42fc31d81ce6d0bc2b3ca5be2faaf
Gatitulina Julia <J.Gatitulina@trkua.tv>
2023-04-18 21:35:01 +0300

    Task_2.py_update

--- Lesson_13/Task_2.py
+++ Lesson_13/Task_2.py
@@ -1,8 +1,8 @@
 #Task2 access a function inside a function (Tips: use function, which returns another function)
 
-def outside_func(q,w):
+def outside_func():
     k = 5
     def sum_func(i, j): return i + j + k
-    return sum_func(q,w)
+    return sum_func
 
-print(outside_func(4,3))
+print(outside_func()(3,4))
